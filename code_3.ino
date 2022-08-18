f85afb2d9ee16d8dd54ef7e43c3580fd7a25299a
Ayaanjain1919 <111034093+Ayaanjain1919@users.noreply.github.com>
2022-08-18 09:24:19 +0530

    Add files via upload

---
+++ code_3.ino
@@ -0,0 +1,33 @@
+const int ledPin = 4;
+  const int led2Pin = 5;
+ 
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin  , OUTPUT );
+pinMode(led2Pin , OUTPUT );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(led2Pin, HIGH);
+delay(2000);
+digitalWrite(led2Pin, LOW);
+delay(2000);
+
+digitalWrite(ledPin, HIGH);
+delay(2000);
+digitalWrite(ledPin, LOW);
+delay(2000);
+
+digitalWrite(led2Pin, HIGH);
+delay(2000);
+digitalWrite(led2Pin, LOW);
+delay(2000);
+
+digitalWrite(ledPin, HIGH);
+delay(2000);
+digitalWrite(ledPin, LOW);
+delay(2000);
+
+}
