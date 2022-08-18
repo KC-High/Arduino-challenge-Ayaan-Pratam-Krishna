1d7c623b2ca99788899fb2c898ad7641e524d1d3
Ayaanjain1919 <111034093+Ayaanjain1919@users.noreply.github.com>
2022-08-18 08:55:46 +0530

    Add files via upload

---
+++ sketch_aug04a.ino
@@ -0,0 +1,19 @@
+const int ledpin = 5;
+int ledstate = LOW;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledpin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+if (ledstate == LOW) {
+  ledstate = HIGH;
+} else {
+ledstate = LOW;
+}
+
+digitalWrite(ledpin, ledstate);
+delay(1000); }
